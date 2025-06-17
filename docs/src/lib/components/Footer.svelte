da3c90f2c2c1ce3ffba0f569d839a3dd0e6778c2
Johan Ronsse <johan.ronsse@gmail.com>
2025-06-17 17:46:45 -0600

    Tweak

--- docs/src/lib/components/Footer.svelte
+++ docs/src/lib/components/Footer.svelte
@@ -1,3 +1,3 @@
-<footer class="p-4">
+<footer class="p-4 md:p2 bg-gray-100">
     <p>Obra shadcn/ui is a UI kit for Figma that tracks shadcn/ui v4. See <a href="https://figma.com/@obra-studio" class="text-gray-800 underline">our Figma community profile</a> to download.</p>
 </footer>
