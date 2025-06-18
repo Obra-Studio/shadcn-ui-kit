31ebbaaa05272b5429f7e42527eb248bab6850bb
Johan Ronsse <johan.ronsse@gmail.com>
2025-06-17 19:51:50 -0600

    Customize typography, don’t use prose plugin

--- docs/src/lib/components/Header.svelte
+++ docs/src/lib/components/Header.svelte
@@ -1,4 +1,4 @@
-<header class="flex mx-auto p-4 md:p-2 w-full bg-white border-0 border-b border-gray-300">
+<header class="flex mx-auto p-4 md:p-8 w-full bg-white border-0 border-b border-gray-300">
     <a href="/" class="text-2xl no-underline inline-flex items-center font-semibold gap-2 text-black">
         <img src="logo-obra.svg" alt="Obra" width="50" />
         <span>shadcn/ui docs</span>
--- docs/src/lib/components/ui/button-group/button-group.svelte
+++ docs/src/lib/components/ui/button-group/button-group.svelte
@@ -4,6 +4,6 @@
 	} = $props();
 </script>
 
-<div class="flex gap-2 items-center">
+<div class="flex gap-2 pb-4 items-center">
 	{@render children()}
 </div>
