b7f2704f9a7df0699d720f9dcbcda51e8db9f637
Johan Ronsse <johan.ronsse@gmail.com>
2025-06-26 12:43:55 -0600

    Remove external options

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -4,9 +4,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
-	build: {
-		rollupOptions: {
-			external: ['sharp','fs','cookie','devalue','clsx','set-cookie-parser'],
-		},
-	},
 });
