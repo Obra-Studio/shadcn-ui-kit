199dc11fd25d0bf05d3193f1464050198a7c5b50
Johan Ronsse <johan.ronsse@gmail.com>
2025-06-30 16:46:30 -0600

    Tweak design

--- docs/src/lib/components/Header.svelte
+++ docs/src/lib/components/Header.svelte
@@ -3,10 +3,12 @@
 </script>
 
 <header class="flex justify-between items-center mx-auto p-4 md:p-8 w-full bg-white border-0 border-b border-gray-300">
-    <a href="/" class="text-2xl no-underline inline-flex items-center font-semibold gap-2 text-black">
-        <img src="/logo-shadcn.svg" alt="shadcn/ui" width="32" />
-        <img src="/logo-obra.svg" alt="Obra" width="50" />
-        <span>shadcn/ui docs</span>
+    <a href="/" class="text-2xl no-underline inline-flex items-center font-semibold gap-3 text-black">
+        <span class="gap-0.5 flex">
+            <img src="/logo-obra.svg" alt="" width="50" />
+            <img src="/logo-shadcn.svg" alt="" width="36" />
+        </span>
+        <span>Obra shadcn/ui docs</span>
     </a>
 
     <!-- @todo sort out the custom breakpoint or use standard breakpoints -->
