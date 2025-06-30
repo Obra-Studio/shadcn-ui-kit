50afd4cd3f0742a4cb4f62b53eac47899227983d
Johan Ronsse <johan.ronsse@gmail.com>
2025-06-30 15:57:40 -0600

    Make links more visible

--- docs/src/routes/+page.svelte
+++ docs/src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
-	import IntroContent from './intro-content.svx'
+	import IntroContent1 from './intro-content-1.svx'
+	import IntroContent2 from './intro-content-2.svx'
+	import {ButtonGroup} from "$lib/components/ui/button-group/index.js";
+	import {Button} from "$lib/components/ui/button/index.js";
 </script>
 
 <svelte:head>
@@ -7,5 +10,14 @@
 </svelte:head>
 
 <div class="prose">
-	<IntroContent />
+	<IntroContent1 />
+</div>
+
+<ButtonGroup>
+	<Button icon="figma" href="https://www.figma.com/community/file/1514746685758799870/obra-shadcn-ui">Duplicate on Figma community</Button>
+	<Button icon="github" variant="secondary" href="https://github.com/Obra-Studio/shadcn-ui-kit/issues">Discuss on Github</Button>
+</ButtonGroup>
+
+<div class="prose">
+	<IntroContent2 />
 </div>
