8564fdf0425bb2f4d7bf67b7ebf88905dd3a7398
Johan Ronsse <johan.ronsse@gmail.com>
2025-09-09 06:34:40 -0600

    Add header link to Figma & Github

--- docs/src/lib/components/Header.svelte
+++ docs/src/lib/components/Header.svelte
@@ -1,5 +1,7 @@
 <script>
     import { IconMenu } from 'obra-icons-svelte'
+    import {Button} from "$lib/components/ui/button/index.js";
+    import {ButtonGroup} from "$lib/components/ui/button-group/index.js";
 </script>
 
 <header class="flex justify-between items-center mx-auto p-4 md:p-8 w-full bg-white border-0 border-b border-gray-300">
@@ -10,6 +12,13 @@
         </span>
         <span>Obra shadcn/ui docs</span>
     </a>
+
+    <div class="hidden md:block">
+        <ButtonGroup>
+            <Button icon="figma" variant="ghost" href="https://www.figma.com/community/file/1514746685758799870/obra-shadcn-ui">Get design file</Button>
+            <Button icon="github" variant="ghost" href="https://github.com/Obra-Studio/shadcn-ui-kit/issues">Give feedback</Button>
+        </ButtonGroup>
+    </div>
 
     <!-- @todo sort out the custom breakpoint or use standard breakpoints -->
     <a href="#nav" class="md:hidden">
