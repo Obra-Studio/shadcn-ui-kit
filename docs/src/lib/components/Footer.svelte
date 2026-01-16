78c545044bf361de7c051b3b400315a0baa24193
Wolfr <johan.ronsse@gmail.com>
2026-01-16 11:14:16 -0600

    value

--- docs/src/lib/components/Footer.svelte
+++ docs/src/lib/components/Footer.svelte
@@ -2,7 +2,7 @@
 	<div class="w-full">
 		<p class="leading-9 mb-0 text-lg text-foreground text-center">
 			Obra shadcn/ui is a completely free, high quality UI kit for Figma
-			loved by over 25,000 creators.
+			loved by over 30,000 creators.
 		</p>
 		<p class="leading-9 text-lg text-foreground text-center">
 			See <a href="https://figma.com/@obrastudio" class="underline"
