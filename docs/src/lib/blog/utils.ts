bcd80d8958aa7a052d51ef7ca2f9a11185d7fe04
Wolfr <johan.ronsse@gmail.com>
2026-01-16 16:46:41 -0600

    Fix issue with dates

--- docs/src/lib/blog/utils.ts
+++ docs/src/lib/blog/utils.ts
@@ -1,6 +1,11 @@
 import { format } from 'date-fns';
 
-export function formatDate(date: Date) {
+export function formatDate(date: Date | string) {
+	// If it's a string like '2026-01-14', parse it as a local date to avoid timezone shift
+	if (typeof date === 'string') {
+		const [year, month, day] = date.split('-').map(Number);
+		date = new Date(year, month - 1, day);
+	}
 	return format(date, 'MMMM do, yyyy');
 }
 
